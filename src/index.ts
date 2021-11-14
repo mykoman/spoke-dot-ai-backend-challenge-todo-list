7aa18ea8bfc54dfb575552b6f360065e18e9ce91
Michael Ogbuma <ogbumamichael@gmail.com>
2021-11-14 05:49:47 +0100

    - Add eslint configuration file - Temporarily mute database connection

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { databaseConnection } from "./config/database";
 config(); 
 const port = process.env.PORT || 3000;
 
-const databaseURL = process.env.DATABASE_URL;
-databaseConnection(databaseURL);
+      const databaseURL = process.env.DATABASE_URL;
+      //databaseConnection(databaseURL);
 
 app.listen(port, ()=>{
   console.log(`App now running on port ${port}`);
