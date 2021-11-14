e875136d8e268ab2b50d5f2b010bb494f9ec9228
Michael Ogbuma <ogbumamichael@gmail.com>
2021-11-14 12:05:53 +0100

    - configure husky

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -3,7 +3,7 @@ import ApplicationError from '../helpers/error-response'
 
 /**
  * @description this middleware validates the request body when http requests are made to the server
- * @param inputFunction Function function by joi to run validation on input parameters
+ * @param  inputFunction function by joi to run validation on input parameters
  * @returns Object
  */
 export const validate = (inputFunction) => {
@@ -11,7 +11,6 @@ export const validate = (inputFunction) => {
         const { error } = inputFunction(req.body)
 
         if (error) {
-            //only one error is handled per time. The details array has one content
             const errorMessage = error.details[0].message
             throw new ApplicationError(400, errorMessage)
         }
