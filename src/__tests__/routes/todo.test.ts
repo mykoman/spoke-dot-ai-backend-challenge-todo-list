e8f8d458975896412f9e1065669a2f2d04fd2a2b
Michael Ogbuma <ogbumamichael@gmail.com>
2021-11-14 14:36:33 +0100

    Refactor build script

--- src/__tests__/routes/todo.test.ts
+++ src/__tests__/routes/todo.test.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv'
 import supertest from 'supertest'
 import app from '../../app'
 import { databaseConnection } from '../../config/database'
+import { Todo } from '../../models/Todo'
 
 config()
 const testApp = () => supertest(app)
@@ -9,6 +10,13 @@ describe('Fetching of todo items from the database', async () => {
     const testDatabaseURL = process.env.TEST_DATABASE_URL
     beforeAll(async () => {
         await databaseConnection(testDatabaseURL)
+        
+        const todo = Todo.create({
+            name,
+            status,
+        })
+    
+        await todo.save()
         await testApp().get('/api/v1/todos/').send()
     })
 
